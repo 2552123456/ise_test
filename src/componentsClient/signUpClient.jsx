872666db2213087368b136e19d7953d69a32dc74
Roie9082 <roie1158@gmail.com>
2024-10-01 10:55:21 +0300

    signUp checking

--- src/componentsClient/signUpClient.jsx
+++ src/componentsClient/signUpClient.jsx
@@ -3,15 +3,30 @@ import React from 'react'
 function SignUpClient() {
 
   return (
-    <div>
-       <h1>SignUp</h1>
-       <p>first name</p>
-       <p>last name</p>
-       <p>email</p>
-       <p>password</p>
-       <button > accept</button>
-       <button  className=''>sign up</button>
-
+    <div className='container signup-container card  mt-5'>
+      <div className='card-body'>
+       <h1 className='text-center'>SignUp</h1>
+       <p className='m-1'>first name</p>
+       <input type="text" className="form-control mb-3 " placeholder="First Name" />
+       <p className='m-1'>last name</p>
+       <input type="text" className="form-control mb-3 " placeholder="last name" />
+       <p className='m-1'>email</p>
+       <input type="email" className="form-control mb-3 " placeholder="email" />
+       <p className='m-1'>password</p>
+       <input type="password" className="form-control mb-3 " placeholder="password" />
+       <div className="form-check">
+      <input type="checkbox" className="form-check-input" id="acceptTerms" />
+      <label className="form-check-label ms-2" htmlFor="acceptTerms">
+        I accept the{' '}
+        <button className="btn btn-link p-0" type="button">
+          Terms of Use & Privacy Policy
+        </button>
+      </label>
+    </div>
+  <br />
+  <br />
+       <button type='submit' className='col-8 alert-link border-2 btn btn-outline-success '>sign up</button>
+</div>
     </div>
   )
 }
